5edcfccad12725d52e1926761acb70314c0e1bb0
prashant708 <74255422+prashant708@users.noreply.github.com>
2025-09-09 21:33:38 +0530

    Update PatientRepository.java

--- app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -2,6 +2,7 @@ package com.project.back_end.repo;
 
 import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
